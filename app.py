06aaaca50b9b2d1adb713f8e8f9f2dec1636fe4d
Tanumoy Bose <tanumoybose1986@gmail.com>
2023-03-16 16:03:00 +0000

    update dynamic containt

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify 
 
 app = Flask(__name__)
 
+PRODUCTS=[
+{
+ 'id' :1,
+  'title':'Hero Cycle',
+  'description':'HeroUrban Terrain UT7001S27.5  City Bike with Complete Accessories, Free Cycling Event & Ride Tracking App by Cultsport (Black - Blue, Ideal for Unisex, Frame: 18 Inches)',
+  'price':2800,
+  
+}, 
+{
+ 'id' :2,
+  'title':'Tata Cycle',
+  'description':'Tata Urban Terrain UT7001S27.5  City Bike with Complete Accessories, Free Cycling Event & Ride Tracking App by Cultsport (Black - Blue, Ideal for Unisex, Frame: 19 Inches)',
+  'price':2500,
+  
+},
+{
+ 'id' :3,
+  'title':'Avon Cycle',
+  'description':'Avon Urban Terrain UT7001S27.5  City Bike with Complete Accessories, Free Cycling Event & Ride Tracking App by Cultsport (Black - Blue, Ideal for Unisex, Frame: 18 Inches)',
+  'price':6000,
+  
+}
+  
+]
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html',products=PRODUCTS)
 
+
+@app.route("/api/products")
+def product_list():
+  return jsonify(PRODUCTS)
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True)
